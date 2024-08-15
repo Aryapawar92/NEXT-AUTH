769bc1059bd3804666b552fd6d179b8643cb0cad
arya <aryapawar92@gmail.com>
2024-08-15 17:07:53 +0530

    first commit

---
+++ src/models/userModels.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Please add a username"],
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    VerifyToken: String,
+    VerifyTokenExpiry: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
